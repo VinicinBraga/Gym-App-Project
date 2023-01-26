b011d2b565482c5245b768e64a6289d40c4817c1
VinicinBraga <vinicius@bragamatta.com.br>
2023-01-26 17:34:37 -0300

    isTopOfPage on Navbar Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Navbar from './components/Navbar/Navbar';
 import { SelectedPage } from './shared/types'
 
@@ -6,10 +6,28 @@ import { SelectedPage } from './shared/types'
 const App = () => {
 
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true)
+        setSelectedPage(SelectedPage.Home)
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false)
+
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, []);
+  
 
   return (
-    <div>
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage} 
+        setSelectedPage={setSelectedPage}  />
     </div>
   )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,20 +7,22 @@ import { useState } from 'react';
 import ActionButton from '../../shared/ActionButton';
 
 type Props = {
+  isTopOfPage: boolean
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({isTopOfPage, selectedPage, setSelectedPage }: Props) => {
  
   const flexBetween = 'flex items-center justify-between';
-  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
-  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const navbarBackground = isTopOfPage ? "" : 'bg-primary-100 drop-shadow';
   
 Link
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <img src={Logo} alt='Logo' />
