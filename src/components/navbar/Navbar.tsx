cfbb6bec63b2e4492715ee9ec10280e7d0331553
VinicinBraga <vinicius@bragamatta.com.br>
2023-01-30 16:04:25 -0300

    New logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -25,7 +25,7 @@ Link
       <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
-            <img src={Logo} alt='Logo' />
+            <img className='w-56' src={Logo} alt='Logo'/>
             {isAboveMediumScreens ? (
               <div className={`${flexBetween} w-full`}>
                 <div className={`${flexBetween} gap-8 text-sm`}>
