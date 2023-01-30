91503534d54e152af0f3d03480a9eb5ac73cf58c
VinicinBraga <vinicius@bragamatta.com.br>
2023-01-30 11:24:48 -0300

    Add motion on Home Component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -7,25 +7,33 @@ import SponsorForbes from '../../assets/SponsorForbes.png';
 import SponsorFortune from '../../assets/SponsorFortune.png';
 import { SelectedPage } from '../../shared/types';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from "framer-motion";
+
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
-}
+};
 
 const Home = ({ setSelectedPage }: Props) => {
-  
-  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)")
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
 
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
-      {/* IMAGE AND MAIN HEADER */}
-      <div
+      <motion.div
         className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
       >
         <div className="z-10 mt-32 md:basis-3/5">
-       
-          <div
+          <motion.div
             className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
           >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
@@ -38,11 +46,18 @@ const Home = ({ setSelectedPage }: Props) => {
               Studios to get the Body Shapes That you Dream of.. Get Your Dream
               Body Now.
             </p>
-          </div>
+          </motion.div>
 
-      
-          <div
+          <motion.div
             className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
           >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
@@ -54,7 +69,7 @@ const Home = ({ setSelectedPage }: Props) => {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
 
         <div
@@ -63,7 +78,7 @@ const Home = ({ setSelectedPage }: Props) => {
         >
           <img alt="home-pageGraphic" src={HomePageGraphic} />
         </div>
-      </div>
+      </motion.div>
 
       {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
